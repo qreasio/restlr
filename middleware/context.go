2f092619109f51204bfce2490e3f943a2a14f50e
qreasio <benpinternet@yahoo.com>
2019-10-15 10:00:50 +0700

    delete unused middleware

--- middleware/context.go
+++
@@ -1,5 +0,0 @@
-package middleware
-
-// ApiKey is a function that will check and validate api key from every http request header
-// Only request that has prefix as specified in allowedPaths will be skipped
-// If the api key is not valid, will return 401 status
